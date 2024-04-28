cccbc44a74be58d024a503cb13f4634cb2cb9ea7
CHUNDURU PIYUSH VARSHA <92862803+piyush318@users.noreply.github.com>
2024-04-27 23:38:37 -0400

    Update and rename My_Solution to Data and Solution.sql

---
+++ Query_25/Data and Solution.sql
@@ -0,0 +1,24 @@
+--Data Set
+drop table if exists product_demo;
+create table product_demo
+(
+	store_id	int,
+	product_1	varchar(50),
+	product_2	varchar(50)
+);
+insert into product_demo values (1, 'Apple - IPhone', '   Apple - MacBook Pro');
+insert into product_demo values (1, 'Apple - AirPods', 'Samsung - Galaxy Phone');
+insert into product_demo values (2, 'Apple_IPhone', 'Apple: Phone');
+insert into product_demo values (2, 'Google Pixel', ' apple: Laptop');
+insert into product_demo values (2, 'Sony: Camera', 'Apple Vision Pro');
+insert into product_demo values (3, 'samsung - Galaxy Phone', 'mapple MacBook Pro');
+
+select * from product_demo;
+
+
+-- Solution
+select store_id, 
+       sum(case when trim(lower(product_1)) like 'apple%' then 1 else 0 end) ,
+       sum(case when trim(lower(product_2)) like 'apple%' then 1 else 0 end)  
+from product_demo
+group by store_id;
